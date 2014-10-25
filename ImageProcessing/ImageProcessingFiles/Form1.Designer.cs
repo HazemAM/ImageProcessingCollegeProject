e839785489f3f85d8d308aea3253d2b68fcd5e54
Hazem AbuMostafa <hbluebrince@gmail.com>
2014-10-25 23:44:22 +0200

    Task 1: Initial transformations layout.
    - Initial layout for supporting transformations of Task 1.
    - Change: Now images are displayed in their real sizes to make scaling
    change visible.

--- ImageProcessing/ImageProcessingFiles/Form1.Designer.cs
+++ ImageProcessing/ImageProcessingFiles/Form1.Designer.cs
@@ -33,11 +33,19 @@ namespace ImageProcessing
             this.btnOpen = new System.Windows.Forms.Button();
             this.toolTip = new System.Windows.Forms.ToolTip(this.components);
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.grpboxTask1 = new System.Windows.Forms.GroupBox();
+            this.btnTransAll = new System.Windows.Forms.Button();
+            this.btnShear = new System.Windows.Forms.Button();
+            this.btnRotate = new System.Windows.Forms.Button();
+            this.btnScale = new System.Windows.Forms.Button();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.leftPictureBox = new System.Windows.Forms.PictureBox();
             this.rightPictureBox = new System.Windows.Forms.PictureBox();
             this.saveDialog = new System.Windows.Forms.SaveFileDialog();
             this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.grpboxTask1.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.leftPictureBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.rightPictureBox)).BeginInit();
@@ -47,7 +55,7 @@ namespace ImageProcessing
             // 
             this.btnOpen.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnOpen.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnOpen.Location = new System.Drawing.Point(394, 442);
+            this.btnOpen.Location = new System.Drawing.Point(391, 5);
             this.btnOpen.Margin = new System.Windows.Forms.Padding(5);
             this.btnOpen.Name = "btnOpen";
             this.btnOpen.Size = new System.Drawing.Size(126, 62);
@@ -64,8 +72,8 @@ namespace ImageProcessing
             // 
             this.tableLayoutPanel2.ColumnCount = 1;
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 0, 1);
             this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel1, 0, 0);
-            this.tableLayoutPanel2.Controls.Add(this.btnOpen, 0, 1);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
@@ -75,6 +83,79 @@ namespace ImageProcessing
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 79F));
             this.tableLayoutPanel2.Size = new System.Drawing.Size(914, 513);
             this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 3;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 138F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.btnOpen, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.grpboxTask1, 0, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 437);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(908, 73);
+            this.tableLayoutPanel3.TabIndex = 3;
+            // 
+            // grpboxTask1
+            // 
+            this.grpboxTask1.Controls.Add(this.btnTransAll);
+            this.grpboxTask1.Controls.Add(this.btnShear);
+            this.grpboxTask1.Controls.Add(this.btnRotate);
+            this.grpboxTask1.Controls.Add(this.btnScale);
+            this.grpboxTask1.Location = new System.Drawing.Point(3, 3);
+            this.grpboxTask1.Name = "grpboxTask1";
+            this.grpboxTask1.Size = new System.Drawing.Size(379, 63);
+            this.grpboxTask1.TabIndex = 2;
+            this.grpboxTask1.TabStop = false;
+            this.grpboxTask1.Text = "Task 1";
+            // 
+            // btnTransAll
+            // 
+            this.btnTransAll.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnTransAll.Location = new System.Drawing.Point(256, 24);
+            this.btnTransAll.Name = "btnTransAll";
+            this.btnTransAll.Size = new System.Drawing.Size(63, 24);
+            this.btnTransAll.TabIndex = 4;
+            this.btnTransAll.Text = "(All)";
+            this.btnTransAll.UseVisualStyleBackColor = true;
+            this.btnTransAll.Click += new System.EventHandler(this.btnTransAll_Click);
+            // 
+            // btnShear
+            // 
+            this.btnShear.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnShear.Location = new System.Drawing.Point(193, 24);
+            this.btnShear.Name = "btnShear";
+            this.btnShear.Size = new System.Drawing.Size(63, 24);
+            this.btnShear.TabIndex = 3;
+            this.btnShear.Text = "Shear";
+            this.btnShear.UseVisualStyleBackColor = true;
+            this.btnShear.Click += new System.EventHandler(this.btnShear_Click);
+            // 
+            // btnRotate
+            // 
+            this.btnRotate.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnRotate.Location = new System.Drawing.Point(130, 24);
+            this.btnRotate.Name = "btnRotate";
+            this.btnRotate.Size = new System.Drawing.Size(63, 24);
+            this.btnRotate.TabIndex = 2;
+            this.btnRotate.Text = "Rotate";
+            this.btnRotate.UseVisualStyleBackColor = true;
+            this.btnRotate.Click += new System.EventHandler(this.btnRotate_Click);
+            // 
+            // btnScale
+            // 
+            this.btnScale.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnScale.Location = new System.Drawing.Point(67, 24);
+            this.btnScale.Name = "btnScale";
+            this.btnScale.Size = new System.Drawing.Size(63, 24);
+            this.btnScale.TabIndex = 1;
+            this.btnScale.Text = "Scale";
+            this.btnScale.UseVisualStyleBackColor = true;
+            this.btnScale.Click += new System.EventHandler(this.btnScale_Click);
             // 
             // tableLayoutPanel1
             // 
@@ -100,7 +181,7 @@ namespace ImageProcessing
             this.leftPictureBox.Margin = new System.Windows.Forms.Padding(0);
             this.leftPictureBox.Name = "leftPictureBox";
             this.leftPictureBox.Size = new System.Drawing.Size(457, 434);
-            this.leftPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.leftPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.leftPictureBox.TabIndex = 0;
             this.leftPictureBox.TabStop = false;
             this.leftPictureBox.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox_DoubleClick);
@@ -114,7 +195,7 @@ namespace ImageProcessing
             this.rightPictureBox.Margin = new System.Windows.Forms.Padding(0);
             this.rightPictureBox.Name = "rightPictureBox";
             this.rightPictureBox.Size = new System.Drawing.Size(457, 434);
-            this.rightPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.rightPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.rightPictureBox.TabIndex = 11;
             this.rightPictureBox.TabStop = false;
             this.rightPictureBox.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox_DoubleClick);
@@ -137,6 +218,8 @@ namespace ImageProcessing
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Image Processing";
             this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.grpboxTask1.ResumeLayout(false);
             this.tableLayoutPanel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.leftPictureBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.rightPictureBox)).EndInit();
@@ -153,6 +236,12 @@ namespace ImageProcessing
         private System.Windows.Forms.PictureBox leftPictureBox;
         private System.Windows.Forms.PictureBox rightPictureBox;
         private System.Windows.Forms.SaveFileDialog saveDialog;
+        private System.Windows.Forms.Button btnScale;
+        private System.Windows.Forms.GroupBox grpboxTask1;
+        private System.Windows.Forms.Button btnShear;
+        private System.Windows.Forms.Button btnRotate;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Button btnTransAll;
     }
 }
 
