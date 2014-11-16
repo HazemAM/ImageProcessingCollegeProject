0a5804ed9f310467ae7ea3cac86803a3f65b0ffb
chaosagex <hk2nasr@gmail.com>
2014-11-16 14:19:48 +0200

    had to made matlab deal with 2d arrays as he seems not able to handle 3d arrays and it worked perfectly :D ps make window size odd only or inc by 2 as even values will give exceps retenix needs some fixes (at least in my old ver as i couldnt use ur dll as u're using newer matlab so dependencies doesnt work for me neither would for the dr :p) make a local branch with ur matlab dependencies and put only working ver in master(that's what i did) have fun reading this and fixing :D

--- ImageProcessing/ImageProcessingFiles/Dll Handler.cs
+++ ImageProcessing/ImageProcessingFiles/Dll Handler.cs
@@ -18,50 +18,65 @@ namespace ImageProcessing
         public Bitmap LocalHistEQ(Bitmap inp, double size)
         {
             Bitmap res = new Bitmap(inp);
-            double[,,]im=getArr(inp);
-            MWArray re=handler1.LocalHE((MWNumericArray)im,size);
-            double[,,]tem=(double[,,])re.ToArray();
-            res=getImg(inp,tem);
+            double[][,] im = getArr(inp);
+            MWArray re = handler1.LocalHE((MWNumericArray)im[0], size);
+            double[,] tem = (double[,])re.ToArray();
+            re = handler1.LocalHE((MWNumericArray)im[1], size);
+            double[,] tem1 = (double[,])re.ToArray();
+            re = handler1.LocalHE((MWNumericArray)im[2], size);
+            double[,] tem2 = (double[,])re.ToArray();
+            res = getImg(inp, tem, tem1, tem2);
             return res;
         }
-        public Bitmap LocalStat(Bitmap inp, double size,double E,double K0,double K1,double K2)
+        public Bitmap LocalStat(Bitmap inp, double size, double E, double K0, double K1, double K2)
         {
             Bitmap res = new Bitmap(inp);
-            double[,,]im=getArr(inp);
-            MWArray re=handler1.LocalStatistics((MWNumericArray)im,size,E,K0,K1,K2);
-            double[, ,] tem = (double[,,])re.ToArray();
-            res=getImg(inp,tem);
+            double[][,] im = getArr(inp);
+            MWArray re = handler1.LocalStatistics((MWNumericArray)im[0], size, E, K0, K1, K2);
+            double[,] tem = (double[,])re.ToArray();
+            re = handler1.LocalStatistics((MWNumericArray)im[1], size, E, K0, K1, K2);
+            double[,] tem1 = (double[,])re.ToArray();
+            re = handler1.LocalStatistics((MWNumericArray)im[2], size, E, K0, K1, K2);
+            double[,] tem2 = (double[,])re.ToArray();
+            res = getImg(inp, tem, tem1, tem2);
             return res;
         }
         public Bitmap Retinex(Bitmap inp, double sigma)
         {
             Bitmap res = new Bitmap(inp);
-            double[, ,] im = getArr(inp);
-            MWArray re = handler1.SSR((MWNumericArray)im, sigma);
-            double[, ,] tem = (double[, ,])re.ToArray();
-            res = getImg(inp, tem);
+            double[][,] im = getArr(inp);
+            MWArray re = handler1.SSR((MWNumericArray)im[0], sigma);
+            double[,] tem = (double[,])re.ToArray();
+            re = handler1.SSR((MWNumericArray)im[1], sigma);
+            double[,] tem1 = (double[,])re.ToArray();
+            re = handler1.SSR((MWNumericArray)im[2], sigma);
+            double[,] tem2 = (double[,])re.ToArray();
+            res = getImg(inp, tem, tem1, tem2);
             return res;
         }
-        private static double[,,] getArr(Bitmap inp)
+        private static double[][,] getArr(Bitmap inp)
         {
-            double[,,] img = new double[inp.Width, inp.Height, 3];
+            double[][,] img = new double[3][,];
+            img[0] = new double[inp.Width, inp.Height];
+            img[1] = new double[inp.Width, inp.Height];
+            img[2] = new double[inp.Width, inp.Height];
             for (int i = 0; i < inp.Width; i++)
                 for (int j = 0; j < inp.Height; j++)
                 {
                     Color temp = inp.GetPixel(i, j);
-                    img[i, j,0] = temp.R;
-                    img[i, j,1] = temp.G;
-                    img[i, j,2] = temp.B;
+                    img[0][i, j] = temp.R;
+                    img[1][i, j] = temp.G;
+                    img[2][i, j] = temp.B;
                 }
             return img;
         }
-        private static Bitmap getImg(Bitmap inp, double[,,] arr)
+        private static Bitmap getImg(Bitmap inp, double[,] arr, double[,] arr1, double[,] arr2)
         {
             Bitmap img = new Bitmap(inp);
             for (int i = 0; i < inp.Width; i++)
                 for (int j = 0; j < inp.Height; j++)
                 {
-                    img.SetPixel(i, j, Color.FromArgb((int)arr[i, j,0], (int)arr[i, j,1], (int)arr[i, j,2]));
+                    img.SetPixel(i, j, Color.FromArgb((int)arr[i, j], (int)arr1[i, j], (int)arr2[i, j]));
                 }
             return img;
         }
